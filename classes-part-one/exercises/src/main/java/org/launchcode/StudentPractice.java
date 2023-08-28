7e2f2e9819cb7848317133170df7ea9a9b4abc1a
Jialing Liu <jialingl.liu@gmail.com>
2023-08-28 16:02:53 -0400

    "Completed exercises of classes part 1."

---
+++ classes-part-one/exercises/src/main/java/org/launchcode/StudentPractice.java
@@ -0,0 +1,14 @@
+package org.launchcode;
+import org.launchcode.Student;
+
+public class StudentPractice {
+    public static void main(String[] args){
+        //instantiate your Student class below
+
+        Student kate = new Student("kate", 5555, 1, 4.0);
+        System.out.println("Student is " + kate.getName());
+
+    }
+
+
+}
